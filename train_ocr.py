fc3e8e8637df01bd9ea8a21b7c5d0e727bba396e
jlel <jlelwtf@gmail.com>
2019-11-28 10:27:04 +0300

    fix train script

--- train_ocr.py
+++ train_ocr.py
@@ -8,8 +8,8 @@ arg_parser.add_argument('--train_dataset_path', default='datasets/ocr_dataset/tr
 arg_parser.add_argument('--val_dataset_path', default='datasets/ocr_dataset/val')
 arg_parser.add_argument('--model_weights_path', default='models/data/ocr_model.pth')
 arg_parser.add_argument('--device', default='cpu')
-arg_parser.add_argument('--batch_size', default=64)
-arg_parser.add_argument('--num_epochs', default=10)
+arg_parser.add_argument('--batch_size', default=64, type=int)
+arg_parser.add_argument('--num_epochs', default=10, type=int)
 
 
 if __name__ == '__main__':
